1b19819ae0b43a2222d27ae6625158a6faca5e2a
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-10-16 15:15:44 +0600

    rifat

---
+++ variablesAndDataTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = 'Rifat'; // string data types.
+$age = 20 ; // integer data types.
+$height = 5.9; // float data types.
+$student = true; // boolean data types.
+
+echo $name . ','. $age . ','. $height. ','. $student;
+
+git add . 
+git commit -m"rifat" 
+git push
+?>
