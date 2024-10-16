7575777cce4e55ed21e2c2501d68d85421b9195e
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2024-10-17 00:42:06 +0600

    rf

---
+++ Comments,echo,print.php
@@ -0,0 +1,14 @@
+<?php
+echo 'rifat', 54, 5.7, true; // multiple data output.
+print 'rifat'; // single data output.
+
+// rifa is a bad boy  //it's single line comments.
+# rifat going to home //it's single line comments.
+
+/*hi
+rifat 
+how         // it's multiple line comments.
+are 
+you*/
+
+?>
---
+++ variablesAndDataTypes.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = 'Rifat'; // string data types.
+$age = 20 ; // integer data types.
+$height = 5.9; // float data types.
+$student = true; // boolean data types.
+
+echo $name . ','. $age . ','. $height. ','. $student;
+?>
